9b58c1afcc01755aa7ba589a4ff4cc1a1b15f25f
Jonathan Carter <borkabrak@gmail.com>
2013-08-15 23:51:07 -0500

    Basic code structure and clock frame drawing

--- clock.js
+++ clock.js
@@ -1,2 +1,51 @@
 $(function(){
+  clock = new Clock();
+  clock.draw();
 });
+
+var width = 500;
+var height = 500;
+
+Clock = function(){
+
+  var my = this;
+
+  my.paper = new Raphael(document.getElementById("clock"), width, height);
+
+  my.center = {
+    x: (my.paper.width / 2),
+    y: (my.paper.height / 2)
+  };
+
+  my.radius = (my.paper.width >= my.paper.height ? my.paper.height : my.paper.width) / 2 - 20;
+
+  my.draw = function(){
+
+    // Frame
+    my.frame = my.paper.circle(my.center.x, my.center.y, my.radius)
+    .attr({
+      'stroke-width'  : 30,
+      'stroke'        : '#963', 
+      'fill'          : '#dde'
+    });
+
+    // Numbers
+    for (var n = 0; n < 12; n++){
+      var x = my.center.x;
+      var y = my.center.y - my.radius + my.frame.attr('stroke-width');
+      my.paper.text(x, y, n.toString()).attr({ 'font-size' : 24 });
+    };
+
+    // Hands
+  };
+
+  my.set_time = function(time){
+  };
+
+  my.start = function(){
+  };
+
+  my.stop = function(){
+  };
+
+};
